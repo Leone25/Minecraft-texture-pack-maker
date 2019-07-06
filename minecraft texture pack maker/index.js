189804164aa3f58840b8640050367b0cffc0fa59
Leone25 <franciscono.enry@gmail.com>
2019-07-06 18:45:16 +0200

    working on the dinamic parts of the page

--- minecraft texture pack maker/index.js
+++ minecraft texture pack maker/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
- 
+const fs = require('fs');
+
+// Text to insert into template
+const sampleText = 'hello world!!!';
+const navbar = ` `;
+
+// Template to accept sampleText
+const filepath = 'index.html';
+var result = `${fs.readFileSync(filepath).toString()}`;
+var result = result;
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send(result);
 })
- 
+
 app.listen(8080);
