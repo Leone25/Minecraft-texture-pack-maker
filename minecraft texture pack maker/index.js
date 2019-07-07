8f4fde2f9c98b4c7fd71bdc46627068c9815f8ca
Leone25 <franciscono.enry@gmail.com>
2019-07-07 10:46:40 +0200

    made the auto compiling of the page working

--- minecraft texture pack maker/index.js
+++ minecraft texture pack maker/index.js
@@ -4,15 +4,26 @@ const fs = require('fs');
 
 // Text to insert into template
 const sampleText = 'hello world!!!';
-const navbar = ` `;
+const navbar = 'lol';
 
 // Template to accept sampleText
-const filepath = 'index.html';
-var result = `${fs.readFileSync(filepath).toString()}`;
-var result = result;
+const filepathindex = 'index.html';
+
+function substituteString(input, stringToChange, substitute) {
+	var n = 0;
+	while (true) {
+		n = input.indexOf(stringToChange, n);
+		if (n == -1) { break; } else {
+			input = input.replace(stringToChange, substitute);
+		}
+	}
+	return input;
+}
 
 app.get('/', function (req, res) {
-  res.send(result);
+	var result = fs.readFileSync(filepathindex).toString();
+	result = substituteString(result, '${navbar}', navbar);
+	res.send(result);
 })
 
 app.listen(8080);
